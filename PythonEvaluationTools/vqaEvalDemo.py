0f6cc9294ed678e7203ddaa10e26b8d8fa1f4b50
wolfezhang <zbopen@hotmail.com>
2023-06-02 14:50:15 +0800

    eval val2014 for ok-vqa answers from ofa(w/o retrival?)

--- PythonEvaluationTools/vqaEvalDemo.py
+++ PythonEvaluationTools/vqaEvalDemo.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import sys
-dataDir = '../../VQA'
+dataDir = '../../vqa'
 sys.path.insert(0, '%s/PythonHelperTools/vqaTools' %(dataDir))
 from vqa import VQA
 from vqaEvaluation.vqaEval import VQAEval
@@ -15,16 +15,16 @@ import os
 versionType ='' # this should be '' when using VQA v2.0 dataset
 taskType    ='OpenEnded' # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
 dataType    ='mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0. 
-dataSubType ='train2014'
-annFile     ='%s/Annotations/%s%s_%s_annotations.json'%(dataDir, versionType, dataType, dataSubType)
+dataSubType ='val2014'
+annFile    ='%s/Annotations/%s%s_%s_annotations.json'%(dataDir, versionType, dataType, dataSubType)
 quesFile    ='%s/Questions/%s%s_%s_%s_questions.json'%(dataDir, versionType, taskType, dataType, dataSubType)
 print(annFile, quesFile)
-imgDir      ='%s/Images/%s/%s/' %(dataDir, dataType, dataSubType)
-resultType  ='fake'
+imgDir      = '/home/wolfe/data_sets/ok_vqa/{}'.format(dataSubType)
+resultType  =''
 fileTypes   = ['results', 'accuracy', 'evalQA', 'evalQuesType', 'evalAnsType'] 
 
 # An example result json file has been provided in './Results' folder.  
-
+# reFile = nlvr/vqa/Results/OpenEnded_mscoco_train2014_fake_results.json
 [resFile, accuracyFile, evalQAFile, evalQuesTypeFile, evalAnsTypeFile] = ['%s/Results/%s%s_%s_%s_%s_%s.json'%(dataDir, versionType, taskType, dataType, dataSubType, \
 resultType, fileType) for fileType in fileTypes]  
 
@@ -76,7 +76,7 @@ if len(evals) > 0:
 
 # plot accuracy for various question types
 plt.bar(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].values(), align='center')
-plt.xticks(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].keys(), rotation='0',fontsize=10)
+plt.xticks(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].keys(), rotation=20,fontsize=10)
 plt.title('Per Question Type Accuracy', fontsize=10)
 plt.xlabel('Question Types', fontsize=10)
 plt.ylabel('Accuracy', fontsize=10)
